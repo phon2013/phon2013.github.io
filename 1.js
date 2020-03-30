ee1411e6d6be699f3218a996c6292b53baeac9f8
phon2013 <62864519+phon2013@users.noreply.github.com>
2020-03-30 20:16:38 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,9 +1,9 @@
 <script>
 function checkpassword(){
-var pwd="1234567";//你和朋友约定的密码，写死的
-var inpwd=document.getElementById("password").value;//获取输入的代码
-if(pwd == inpwd){ //判断一下是否一样
-window.location.href="你的url地址，也就是你想让别让你跳到的页面";
+var pwd="1234567";//
+var inpwd=document.getElementById("password").value;//
+if(pwd == inpwd)
+window.location.href="www";
 }
 }
 </script>
